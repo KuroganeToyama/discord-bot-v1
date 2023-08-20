eaa4be622a73db6f70f4eaa57eeaadfe247a6cb2
Di Nguyen <dingyen96@gmail.com>
2023-08-20 17:58:09 -0400

    Updated dice-roll.js

--- commands/fun/dice-roll.js
+++ commands/fun/dice-roll.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,6 +12,46 @@ module.exports = {
     async execute(interaction) {
         const sides = interaction.options.getInteger('integer');
         const result = Math.floor(Math.random() * sides) + 1;
-        await interaction.reply(`You rolled an ${sides}-sided dice. You got a ${result}!`);
+
+        const reroll = new ButtonBuilder()
+            .setCustomId('reroll')
+            .setLabel('Reroll')
+            .setStyle(ButtonStyle.Primary);
+
+        const cancel = new ButtonBuilder()
+            .setCustomId('cancel')
+            .setLabel('Cancel')
+            .setStyle(ButtonStyle.Secondary);
+
+        const row = new ActionRowBuilder()
+            .addComponents(reroll, cancel);
+
+        const response = await interaction.reply({
+            content: `You rolled a ${sides}-dice. You got ${result}!`,
+            components: [row],
+        });
+        
+        const collectionFilter = i => i.user.id == interaction.user.id;
+
+        while (true) {
+            try {
+                const confirmation = await response.awaitMessageComponent({ filter: collectionFilter, time: 60000 });
+                
+                if (confirmation.customId == 'reroll') {
+                    const result = Math.floor(Math.random() * sides) + 1;
+                    await confirmation.update({ content: `You rolled a ${sides}-dice. You got ${result}!`,
+                                                components: [row] });
+                }
+                else if (confirmation.customId == 'cancel') {
+                    await confirmation.update({ content: `No more rolling...`,
+                                                components: [] });
+                    return;
+                }
+            }
+            catch (error) {
+                await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+                return;
+            }
+        }
     },
 };
