a0a10d3ce645d8fdc6a938b282cd0d7ff5e6d027
Di Nguyen <dingyen96@gmail.com>
2023-08-21 12:54:10 -0400

    Added anime-girl.js

---
+++ commands/fun/anime-girl.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('waifu')
+        .setDescription('Random image of anime girls UwU')
+        .addStringOption(option => 
+            option.setName('safe')
+                .setDescription('Choose sfw or nsfw')
+                .setRequired(true))
+        .addStringOption(option => 
+            option.setName('tag')
+                .setDescription('Choose a tag')
+                .setRequired(true)),
+    
+    async execute(interaction) {
+        const root = 'https://api.waifu.pics/';
+        const safe = interaction.options.getString('safe');
+        const tag = interaction.options.getString('tag');
+        const apiCall = root.concat(safe, '/', tag);
+        const waifu = await fetch(apiCall);
+        const json = await waifu.json();
+        await interaction.editReply(json.url);
+    },
+};
