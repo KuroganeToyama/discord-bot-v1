f058d24cd1b2f67b2501fb606ea83f478b033cc9
Di Nguyen <dingyen96@gmail.com>
2023-12-31 16:34:45 -0500

    Added subcommands help and find

--- commands/fun/anime-girl.js
+++ commands/fun/anime-girl.js
@@ -5,28 +5,39 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('waifu')
         .setDescription('Random image of anime girls UwU')
-        .addStringOption(option => 
-            option.setName('safe')
-                .setDescription('Choose sfw or nsfw')
-                .setRequired(true))
-        .addStringOption(option => 
-            option.setName('tag')
-                .setDescription('Choose a tag')
-                .setRequired(true)),
+        .addSubcommand(subcommand => 
+            subcommand.setName('help')
+                .setDescription('List all the tags you can use'))
+        .addSubcommand(subcommand => 
+            subcommand.setName('find')
+                .setDescription('Search for kawaii anime girl arts!')
+                .addStringOption(option => 
+                    option.setName('safe')
+                        .setDescription('Choose sfw or nsfw')
+                        .setRequired(true))
+                .addStringOption(option => 
+                    option.setName('tag')
+                        .setDescription('Choose a tag')
+                        .setRequired(true))),
     
     async execute(interaction) {
         try {
-            const root = 'https://api.waifu.pics/';
-            const safe = interaction.options.getString('safe');
-            const tag = interaction.options.getString('tag');
-            const apiCall = root.concat(safe, '/', tag);
-            const response = await fetch(apiCall);
-            const json = await response.json();
-            const image = (safe === 'nsfw') ? (`|| ${json.url} ||`) : (json.url);
-            await interaction.editReply(image);
+            if (interaction.options.getSubcommand() === 'find') {
+                const root = 'https://api.waifu.pics/';
+                const safe = interaction.options.getString('safe');
+                const tag = interaction.options.getString('tag');
+                const apiCall = root.concat(safe, '/', tag);
+                const response = await fetch(apiCall);
+                const json = await response.json();
+                const image = (safe === 'nsfw') ? (`|| ${json.url} ||`) : (json.url);
+                await interaction.editReply(image);
+            }
+            else if (interaction.options.getSubcommand() === 'help') {
+                await interaction.editReply("waifu, neko, shinobu, megumin, bully, cuddle");
+            }
         }
         catch (error) {
-            await interaction.editReply("You mistyped something. Please try again");
+            await interaction.editReply("Unable to retrieve image.");
             return;
         }
     },
